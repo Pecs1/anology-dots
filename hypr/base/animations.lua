5dd0ee031fa167e65285757bc9523ba1daf85c3c
Pecs1 <173087731+Pecs1@users.noreply.github.com>
2026-06-23 22:49:21 +0200

    small change

--- hypr/base/animations.lua
+++ hypr/base/animations.lua
@@ -15,9 +15,13 @@ hl.config({
         resize_on_border = false,
 
         -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-        allow_tearing = false,
+        allow_tearing = true,
+    },
 
-        layout = "dwindle",
+    dwindle = {
+        preserve_split = true,
+        smart_split = false,
+        smart_resizing = false
     },
 
     decoration = {
